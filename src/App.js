18a7aa9205ccda23f819480e6d5c3f88b3632cb8
Kaio <kaio_smoreira@hotmail.com>
2021-02-04 20:34:59 -0300

    repository

--- src/App.js
+++ src/App.js
@@ -35,9 +35,11 @@ export default function App() {
         return repository;
       }
     });
+
+    setRepositories([repositoryId]);
   }
 
-  setRepositories([...repositories, likeId])
+  
 
   return (
     <>
